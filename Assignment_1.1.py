049fa50865c8d450d6e0b41790e7681038d5df19
Abul-07 <92680357+Abul-07@users.noreply.github.com>
2021-10-17 20:50:51 +0530

    Update Assignment_1.1.py

--- Assignment_1.1.py
+++ Assignment_1.1.py
@@ -4,24 +4,23 @@ print("2. Subtraction")
 print("3. Multiplication")
 print("4. Division")
 
-while True:
-    select = input("Enter any of the above operation (1,2,3,4): ")
+select = input("Enter any of the above operation (1,2,3,4): ")
 
-    if select in ('1', '2', '3', '4'):
-        num1 = int(input("Enter first number: "))
-        num2 = int(input("Enter second number: "))
+if select in ('1', '2', '3', '4'):
+    num1 = int(input("Enter first number: "))
+    num2 = int(input("Enter second number: "))
 
-        if select == '1':
-            print(num1, "+", num2, "=", num1 + num2 )
+     if select == '1':
+        print(num1, "+", num2, "=", num1 + num2 )
 
-        elif select == '2':
-            print(num1, "-", num2, "=", num1 - num2 )
+      elif select == '2':
+         print(num1, "-", num2, "=", num1 - num2 )
 
-        elif select == '3':
-            print(num1, "*", num2, "=", num1 * num2 )
+      elif select == '3':
+         print(num1, "*", num2, "=", num1 * num2 )
 
-        elif select == '4':
-            print(num1, "/", num2, "=", num1 / num2 )
+      elif select == '4':
+         print(num1, "/", num2, "=", num1 / num2 )
     
-    else:
-        print("Invalid Input")
+ else:
+    print("Invalid Input")
