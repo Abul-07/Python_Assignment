7cc68dc396b0564d414227d80b65c95e4070ce5c
Abul-07 <92680357+Abul-07@users.noreply.github.com>
2021-10-24 23:15:19 +0530

    Add files via upload

---
+++ Assignment_2..1.py
@@ -0,0 +1,15 @@
+k = 5 - 1
+ 
+for i in range(0, 5):
+     
+    for j in range(0, k):
+        
+        print(end=" ")
+    
+    k = k - 1
+     
+    for j in range(0, i+1):
+         
+        print("* ", end="")
+     
+    print("")
